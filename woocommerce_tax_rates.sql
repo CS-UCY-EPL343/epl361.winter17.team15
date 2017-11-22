b75e8fbe2ce64d7da69f061a6f32a28df031aaf0
GeorgeStavrou <32328561+GeorgeStavrou@users.noreply.github.com>
2017-11-22 19:19:29 +0200

    Add files via upload

---
+++ woocommerce_tax_rates.sql
@@ -0,0 +1,32 @@
+--
+-- Table structure for table `wp_woocommerce_tax_rates`
+--
+
+CREATE TABLE IF NOT EXISTS `wp_woocommerce_tax_rates` (
+  `tax_rate_id` bigint(20) unsigned NOT NULL,
+  `tax_rate_country` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `tax_rate_state` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `tax_rate` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `tax_rate_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `tax_rate_priority` bigint(20) unsigned NOT NULL,
+  `tax_rate_compound` int(1) NOT NULL DEFAULT '0',
+  `tax_rate_shipping` int(1) NOT NULL DEFAULT '1',
+  `tax_rate_order` bigint(20) unsigned NOT NULL,
+  `tax_rate_class` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `wp_woocommerce_tax_rate_locations`
+--
+
+CREATE TABLE IF NOT EXISTS `wp_woocommerce_tax_rate_locations` (
+  `location_id` bigint(20) unsigned NOT NULL,
+  `location_code` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tax_rate_id` bigint(20) unsigned NOT NULL,
+  `location_type` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
